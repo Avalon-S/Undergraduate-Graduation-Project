9e25089bf8235fb3de4bac8770db1324fc303803
Avalon-S <61087083+Avalon-S@users.noreply.github.com>
2023-05-28 20:35:01 +0800

    Add files via upload

---
+++ Undergraduate-Graduation-Project/model/attention_modules.py
@@ -0,0 +1,168 @@
+import torch
+import torch.nn as nn
+from torch.nn import Module, Conv2d, Parameter
+
+###################Res_NL####################
+class Res_NL(nn.Module):
+    def __init__(self, channel,alpha=0.0):
+        super(Res_NL, self).__init__()
+        self.inter_channel = channel // 2
+        self.conv_phi = nn.Conv2d(in_channels=channel, out_channels=self.inter_channel, kernel_size=1, stride=1,padding=0, bias=False)
+        self.conv_theta = nn.Conv2d(in_channels=channel, out_channels=self.inter_channel, kernel_size=1, stride=1, padding=0, bias=False)
+        self.conv_g = nn.Conv2d(in_channels=channel, out_channels=self.inter_channel, kernel_size=1, stride=1, padding=0, bias=False)
+        self.softmax = nn.Softmax(dim=1)
+        self.conv_mask = nn.Conv2d(in_channels=self.inter_channel, out_channels=channel, kernel_size=1, stride=1, padding=0, bias=False)
+        
+        self.alpha = Parameter(torch.ones(1)*alpha)
+        self.tanh = nn.Tanh()
+
+    def forward(self, x):
+        # [N, C, H , W]
+        b, c, h, w = x.size()
+        # [N, C/2, H * W]
+        x_phi = self.conv_phi(x).contiguous().view(b, c, -1)
+        # [N, H * W, C/2]
+        x_theta = self.conv_theta(x).contiguous().view(b, c, -1).permute(0, 2, 1).contiguous()
+        x_g = self.conv_g(x).contiguous().view(b, c, -1).permute(0, 2, 1).contiguous()
+        # [N, H * W, H * W]
+        mul_theta_phi = torch.matmul(x_theta, x_phi)
+        mul_theta_phi = self.softmax(mul_theta_phi)
+        # [N, H * W, C/2]
+        mul_theta_phi_g = torch.matmul(mul_theta_phi, x_g)
+        # [N, C/2, H, W]
+        mul_theta_phi_g = mul_theta_phi_g.permute(0,2,1).contiguous().view(b,self.inter_channel, h, w)
+        # [N, C, H , W]
+        mask = self.conv_mask(mul_theta_phi_g)
+        out = mask + x
+        
+        out = self.tanh(self.alpha)*out + x
+        
+        return out
+
+###################Res_SE####################
+class Res_SE(nn.Module):
+    def __init__(self, ch_in, reduction=16,alpha=0.0):
+        super(Res_SE, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)  # 全局自适应池化
+        self.fc = nn.Sequential(
+            nn.Linear(ch_in, ch_in // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(ch_in // reduction, ch_in, bias=False),
+            
+            nn.Sigmoid()
+        )
+        
+        self.alpha = Parameter(torch.ones(1) * alpha)
+        
+        self.tanh = nn.Tanh()
+        
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c) # squeeze操作
+        y = self.fc(y).view(b, c, 1, 1) # FC获取通道注意力权重，是具有全局信息的
+        
+        out = x * y.expand_as(x)# 注意力作用每一个通道上
+        
+        out = self.tanh(self.alpha)*out + x
+        
+        return  out
+    
+###################Res_CBAM####################
+class Res_CBAM(nn.Module):
+    def __init__(self,in_channel,reduction=16,kernel_size=7,alpha=0.0):
+        super(Res_CBAM, self).__init__()
+        #通道注意力机制
+        self.max_pool=nn.AdaptiveMaxPool2d(output_size=1)
+        self.avg_pool=nn.AdaptiveAvgPool2d(output_size=1)
+        self.mlp=nn.Sequential(
+            nn.Linear(in_features=in_channel,out_features=in_channel//reduction,bias=False),
+            nn.ReLU(),
+            nn.Linear(in_features=in_channel//reduction,out_features=in_channel,bias=False)
+        )
+        
+        #空间注意力机制
+        self.conv=nn.Conv2d(in_channels=2,out_channels=1,kernel_size=kernel_size ,stride=1,padding=kernel_size//2,bias=False)
+        self.sigmoid=nn.Sigmoid()
+        
+        self.alpha = Parameter(torch.ones(1)*alpha)
+        self.tanh=nn.Tanh()
+
+    def forward(self,x):
+        #通道注意力机制
+        maxout=self.max_pool(x)
+        maxout=self.mlp(maxout.view(maxout.size(0),-1))
+        avgout=self.avg_pool(x)
+        avgout=self.mlp(avgout.view(avgout.size(0),-1))
+        channel_out=self.sigmoid(maxout+avgout)
+        channel_out=channel_out.view(x.size(0),x.size(1),1,1)
+        channel_out=channel_out*x
+        #空间注意力机制
+        max_out,_=torch.max(channel_out,dim=1,keepdim=True)
+        mean_out=torch.mean(channel_out,dim=1,keepdim=True)
+        out=torch.cat((max_out,mean_out),dim=1)
+        out=self.sigmoid(self.conv(out))
+        out=out*channel_out
+        
+        out = self.tanh(self.alpha)*out + x
+        
+        return out
+
+###################Res_CA####################
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+class Res_CA(nn.Module):
+    def __init__(self, inp, reduction=32,alpha=0.0):
+        super(Res_CA, self).__init__()
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+
+        mip = max(8, inp // reduction)
+
+        self.conv1 = nn.Conv2d(inp, mip, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(mip)
+        self.act = h_swish()
+        
+        self.conv_h = nn.Conv2d(mip, inp, kernel_size=1, stride=1, padding=0)
+        self.conv_w = nn.Conv2d(mip, inp, kernel_size=1, stride=1, padding=0)
+        
+        self.alpha = Parameter(torch.ones(1)*alpha)
+        
+        self.tanh = nn.Tanh()
+
+    def forward(self, x):
+        identity = x
+        
+        n,c,h,w = x.size()
+        x_h = self.pool_h(x)
+        x_w = self.pool_w(x).permute(0, 1, 3, 2)
+
+        y = torch.cat([x_h, x_w], dim=2)
+        y = self.conv1(y)
+        y = self.bn1(y)
+        y = self.act(y) 
+        
+        x_h, x_w = torch.split(y, [h, w], dim=2)
+        x_w = x_w.permute(0, 1, 3, 2)
+
+        a_h = self.conv_h(x_h).sigmoid()
+        a_w = self.conv_w(x_w).sigmoid()
+
+        out = identity * a_w * a_h
+        
+        out = self.tanh(self.alpha)*out + x
+        
+        return out
